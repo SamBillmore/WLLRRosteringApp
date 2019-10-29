f805b2f0c0c70f87db301e65448171500b231e28
SamBillmore <sbillmore@uk.ey.com>
2019-10-29 12:26:02 +0000

    Adding test data, plus started creating available

--- Roster_app.py
+++ Roster_app.py
@@ -1,23 +1,27 @@
-import os
-
 from data_imports_and_templates import Timetable
 from data_imports_and_templates import Crew_Requirements
 from data_imports_and_templates import Availability_Form
 from data_imports_and_templates import Blank_Roster
 from data_imports_and_templates import Crew_Member
+from data_imports_and_templates import Crew_Members
 from data_imports_and_templates import Master_Availability
+from data_imports_and_templates import Master_Roster
 
 # Dummy inputs
 
-timetable_path = 'C://Users//sambi//OneDrive//Documents//WLLR//Rostering program//Test data//Timetable.csv'
-crew_reqs_path = 'C://Users//sambi//OneDrive//Documents//WLLR//Rostering program//Test data//Crew requirements.csv'
+timetable_path = './/Test input data//Timetable.csv'
+crew_reqs_path = './/Test input data//Crew requirements.csv'
 
-avail_save_location = 'C://Users//sambi//OneDrive//Documents//WLLR//Rostering program//Test data//Test_Avail_Form.xlsx'
-blank_roster_save_location = 'C://Users//sambi//OneDrive//Documents//WLLR//Rostering program//Test data//Test_Blank_Roster.xlsx'
+avail_save_location = './/Test output data//Test_Avail_Form.xlsx'
+blank_roster_save_location = './/Test output data//Test_Blank_Roster.xlsx'
 
-grade = 'driver'
-availability_folder = 'C://Users//sambi//OneDrive//Documents//WLLR//Rostering program//Test data//01 Driver availability'
-master_roster_save_location = 'C://Users//sambi//OneDrive//Documents//WLLR//Rostering program//Test data//Test_Master_roster.xlsx'
+availability_folders = {
+    'Driver':'.//Test input data//01 Driver availability',
+    'Fireman':'.//Test input data//02 Fireman availability',
+    'Trainee':'.//Test input data//03 Trainee availability'
+}
+master_roster_save_location = './/Test output data//Test_Master_roster.xlsx'
+master_availability_save_location = './/Test output data//Test_Master_availability.xlsx'
 
 
 def create_blank_availability(timetable_path, save_location):
@@ -41,23 +45,25 @@ def create_blank_roster(timetable_path, crew_reqs_path, save_location):
     crew_reqs.import_data(crew_reqs_path)
     blank_roster.create_blank_roster(timetable.data_import,crew_reqs.data_import,save_location)
 
-def import_availability(grade, availability_folder):
+def create_master_availability(availability_folders, save_location):
     """
-    Importing availability from completed availability forms
+    Creating master availability
     """
-    crew_members = []
+    sheet_name = 'master_availability'
+    crew_members = Crew_Members()
     master_availability = Master_Availability()
-    for file_name in os.listdir(availability_folder):
-        crew_member = Crew_Member()
-        crew_member.name = file_name.split('.')[0]
-        crew_member.grade = grade
-        file_path = os.path.join(availability_folder + "/" + file_name)
-        crew_member.import_data(file_path)
-        master_availability.append_availability(crew_member)
-        crew_members.append(crew_member)
-    return crew_members, master_availability
+    for key,value in  availability_folders.items():
+        master_availability.create_master_availability(key, value, crew_members)
+    master_availability.export_data(filepath=save_location,sheet_name=sheet_name)
+    print(crew_members.data)
+    print(master_availability.data_export)
 
+# -------------------------------------------------------------------------------------------------------------
+# Calls below
 
-create_blank_availability(timetable_path,avail_save_location)
-create_blank_roster(timetable_path,crew_reqs_path,blank_roster_save_location)
-crew_members, master_availability = import_availability(grade, availability_folder)
+# create_blank_availability(timetable_path,avail_save_location)
+
+# create_blank_roster(timetable_path,crew_reqs_path,blank_roster_save_location)
+
+create_master_availability(availability_folders,master_availability_save_location)
+
