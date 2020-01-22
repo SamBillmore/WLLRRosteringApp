298dc2322adf13bb9fb565c90b7567e714f76efa
SamBillmore <sbillmore@uk.ey.com>
2020-01-22 15:07:48 +0000

    Updates from testing

--- individual_rosters/create_individual_rosters.py
+++ individual_rosters/create_individual_rosters.py
@@ -32,5 +32,16 @@ class Individual_Rosters(Data_Imports,Data_Exports):
                 fireman_filter = self.data_import['Fireman'] == indiv
                 trainee_filter = self.data_import['Trainee'] == indiv
                 indiv_roster_df = self.data_import[driver_filter | fireman_filter | trainee_filter]
+                indiv_roster_df = self.change_date_format(indiv_roster_df,'Date')
                 indiv_save_path = os.path.join(save_location,r'Individual roster_'+indiv+'.pdf')
                 self.print_df_to_pdf(indiv_roster_df,indiv_save_path)
+
+    def change_date_format(self,df,date_column):
+        """
+        Amends the date format of a date column in a pandas dataframe to:
+        Day of week, Day, Month, Year
+        and stores as a string column
+        """
+        output_df = df.copy()
+        output_df[date_column] = df[date_column].dt.strftime('%a %d %m %Y')
+        return output_df
