700679c09b16ffc5ec770d3331de22d88c5476fe
SamBillmore <sam.billmore@gmail.com>
2023-10-26 17:23:13 +0100

    feat: Updating scaling for full screen

--- src/user_interface/allocate_crews_screen.py
+++ src/user_interface/allocate_crews_screen.py
@@ -104,26 +104,30 @@ class AllocateCrewsScreen(Frame):
             command=lambda: self.controller.show_frame("HomeScreen"),
         )
 
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure(2, minsize=90)
+        self.grid_columnconfigure(0, weight=2)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(2, minsize=350)
+        self.grid_columnconfigure(4, weight=2)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(8, weight=3)
 
         self.instruction_label.grid(
-            row=0, column=0, sticky="W", padx=25, pady=20, columnspan=2
+            row=1, column=1, sticky="W", padx=25, pady=20, columnspan=2
         )
-        self.driver_avail_label.grid(row=1, column=0, sticky="W", padx=25, pady=0)
-        self.driver_avail_entry.grid(row=1, column=1, sticky="EW", padx=0, pady=0)
-        self.driver_avail_button.grid(row=1, column=2, sticky="W", padx=5, pady=0)
-        self.fireman_avail_label.grid(row=2, column=0, sticky="W", padx=25, pady=0)
-        self.fireman_avail_entry.grid(row=2, column=1, sticky="EW", padx=0, pady=0)
-        self.fireman_avail_button.grid(row=2, column=2, sticky="W", padx=5, pady=0)
-        self.trainee_avail_label.grid(row=3, column=0, sticky="W", padx=25, pady=0)
-        self.trainee_avail_entry.grid(row=3, column=1, sticky="EW", padx=0, pady=0)
-        self.trainee_avail_button.grid(row=3, column=2, sticky="W", padx=5, pady=0)
-        self.blank_roster_label.grid(row=4, column=0, sticky="W", padx=25, pady=20)
-        self.blank_roster_entry.grid(row=4, column=1, sticky="EW", padx=0, pady=0)
-        self.blank_roster_button.grid(row=4, column=2, sticky="W", padx=5, pady=0)
-        self.create_timetable_button.grid(row=5, column=1, sticky="E", padx=0, pady=15)
-        self.back_button.grid(row=6, column=0, sticky="W", padx=25, pady=20)
+        self.driver_avail_label.grid(row=2, column=1, sticky="W", padx=25, pady=0)
+        self.driver_avail_entry.grid(row=2, column=2, sticky="EW", padx=0, pady=0)
+        self.driver_avail_button.grid(row=2, column=3, sticky="W", padx=5, pady=0)
+        self.fireman_avail_label.grid(row=3, column=1, sticky="W", padx=25, pady=0)
+        self.fireman_avail_entry.grid(row=3, column=2, sticky="EW", padx=0, pady=0)
+        self.fireman_avail_button.grid(row=3, column=3, sticky="W", padx=5, pady=0)
+        self.trainee_avail_label.grid(row=4, column=1, sticky="W", padx=25, pady=0)
+        self.trainee_avail_entry.grid(row=4, column=2, sticky="EW", padx=0, pady=0)
+        self.trainee_avail_button.grid(row=4, column=3, sticky="W", padx=5, pady=0)
+        self.blank_roster_label.grid(row=5, column=1, sticky="W", padx=25, pady=20)
+        self.blank_roster_entry.grid(row=5, column=2, sticky="EW", padx=0, pady=0)
+        self.blank_roster_button.grid(row=5, column=3, sticky="W", padx=5, pady=0)
+        self.create_timetable_button.grid(row=6, column=2, sticky="E", padx=0, pady=15)
+        self.back_button.grid(row=7, column=1, sticky="W", padx=25, pady=20)
 
     @handle_errors
     def run_create_master_roster(
--- src/user_interface/blank_availability_screen.py
+++ src/user_interface/blank_availability_screen.py
@@ -55,19 +55,23 @@ class BlankAvailabilityScreen(Frame):
             command=lambda: self.controller.show_frame("HomeScreen"),
         )
 
-        self.grid_columnconfigure(1, weight=1)
-        self.grid_columnconfigure(2, minsize=90)
+        self.grid_columnconfigure(0, weight=2)
+        self.grid_columnconfigure(2, weight=1)
+        self.grid_columnconfigure(2, minsize=350)
+        self.grid_columnconfigure(4, weight=2)
+        self.grid_rowconfigure(0, weight=1)
+        self.grid_rowconfigure(6, weight=3)
 
         self.intro_label.grid(
-            row=0, column=0, sticky="W", padx=25, pady=20, columnspan=2
+            row=1, column=1, sticky="W", padx=25, pady=20, columnspan=2
         )
-        self.timetable_label.grid(row=1, column=0, sticky="W", padx=25, pady=20)
-        self.timetable_entry.grid(row=1, column=1, sticky="EW", padx=0, pady=0)
-        self.timetable_path.grid(row=1, column=2, sticky="W", padx=5, pady=0)
+        self.timetable_label.grid(row=2, column=1, sticky="W", padx=25, pady=20)
+        self.timetable_entry.grid(row=2, column=2, sticky="EW", padx=0, pady=0)
+        self.timetable_path.grid(row=2, column=3, sticky="W", padx=5, pady=0)
         self.create_blank_avail_button.grid(
-            row=2, column=1, sticky="E", padx=0, pady=15
+            row=3, column=2, sticky="E", padx=0, pady=15
         )
-        self.back_button.grid(row=3, column=0, sticky="W", padx=25, pady=20)
+        self.back_button.grid(row=4, column=1, sticky="W", padx=25, pady=20)
 
     @handle_errors
     def run_create_blank_availability(self, timetable_path):
