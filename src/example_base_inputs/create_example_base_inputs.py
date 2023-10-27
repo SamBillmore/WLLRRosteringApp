945c69d001425215e9115c9dcfa443449f1186e7
SamBillmore <sam.billmore@gmail.com>
2023-10-27 12:00:16 +0100

    feat: Add example input folders to example input files

--- src/example_base_inputs/create_example_base_inputs.py
+++ src/example_base_inputs/create_example_base_inputs.py
@@ -21,6 +21,7 @@ class ExampleBaseInputs(DataExports):
     def create_base_inputs_controller(self, save_location):
         self.create_example_timetable(save_location=save_location)
         self.create_example_crew_reqs(save_location=save_location)
+        self.create_availability_folders(save_location=save_location)
 
     def create_example_timetable(self, save_location):
         sheet_name = "Timetable"
@@ -60,3 +61,13 @@ class ExampleBaseInputs(DataExports):
         )
         filepath = os.path.join(save_location, "example_crew_reqs_by_colour.xlsx")
         self.export_data(filepath=filepath, sheet_name=sheet_name)
+
+    def create_availability_folders(self, save_location):
+        folders_to_create = [
+            StandardLabels.driver,
+            StandardLabels.fireman,
+            StandardLabels.trainee,
+        ]
+        for folder in folders_to_create:
+            path_to_create = os.path.join(save_location, folder + " availability")
+            os.mkdir(path_to_create)
